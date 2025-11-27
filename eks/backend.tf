6e20ce5394199ca81e7a0d6f7d22408a49f17e2e
Bilal Osmany <mohammedbilalosmany@gmail.com>
2025-11-27 17:09:34 +0530

    Update Terraform required version constraint

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,11 +1,12 @@
 terraform {
-  required_version = "~> 1.9.3"
+  required_version = ">= 1.9.3, < 2.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
   }
+
   backend "s3" {
     bucket         = "dev-bilal-tf-bucket"
     region         = "us-east-1"
@@ -13,8 +14,4 @@ terraform {
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
-}
-
-provider "aws" {
-  region = var.aws-region
 }
